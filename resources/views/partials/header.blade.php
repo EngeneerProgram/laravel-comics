a0231049cbbb9bea3b10280cd0e8f33f0bf9f18d
Rosario Garufi <saro.garufi1@gmail.com>
2022-06-11 14:44:25 +0200

    aggiunta classe active al link attivo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,42 +8,42 @@
     <nav>
         <ul class="menu ">
             <li>
-                <a href="{{route('characters')}}">CHARACTERS</a>
+                <a class="{{Route:: currentRouteName() === 'characters' ? 'active' : ''}}" href="{{route('characters')}}">CHARACTERS</a>
             </li>
             <li>
-            <a href="{{route('comics')}}">COMICS</a>
+            <a class="{{Route:: currentRouteName() === 'comics' ? 'active' : ''}}" href="{{route('comics')}}">COMICS</a>
 
             </li>
             <li>
-            <a href="{{route('movies')}}">MOVIES</a>
+            <a class="{{Route:: currentRouteName() === 'movies' ? 'active' : ''}}" href="{{route('movies')}}">MOVIES</a>
 
             </li>
             <li>
-            <a href="{{route('tv')}}">TV</a>
+            <a class="{{Route:: currentRouteName() === 'tv' ? 'active' : ''}}" href="{{route('tv')}}">TV</a>
 
             </li>
             <li>
-            <a href="{{route('games')}}">GAMES</a>
+            <a class="{{Route:: currentRouteName() === 'games' ? 'active' : ''}}" href="{{route('games')}}">GAMES</a>
 
             </li>
             <li>
-            <a href="{{route('collectbles')}}">COLLECTIBLES</a>
+            <a class="{{Route:: currentRouteName() === 'collectbles' ? 'active' : ''}}" href="{{route('collectbles')}}">COLLECTIBLES</a>
 
             </li>
             <li>
-            <a href="{{route('videos')}}">VIDEOS</a>
+            <a class="{{Route:: currentRouteName() === 'videos' ? 'active' : ''}}" href="{{route('videos')}}">VIDEOS</a>
 
             </li>
             <li>
-            <a href="{{route('fans')}}">FANS</a>
+            <a class="{{Route:: currentRouteName() === 'fans' ? 'active' : ''}}" href="{{route('fans')}}">FANS</a>
 
             </li>
             <li>
-            <a href="{{route('news')}}">NEWS</a>
+            <a class="{{Route:: currentRouteName() === 'news' ? 'active' : ''}}" href="{{route('news')}}">NEWS</a>
 
             </li>
             <li>
-            <a href="{{route('shop')}}">SHOP</a>
+            <a class="{{Route:: currentRouteName() === 'shop' ? 'active' : ''}}" href="{{route('shop')}}">SHOP</a>
 
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('characters');
+    
 })->name('characters');
 
 Route::get('/comics', function () {
